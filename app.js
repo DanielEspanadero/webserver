246a7a219bc3f7a99d5fce749baff65063add5ce
Daniel Españadero <developer@daniel-espanadero.com>
2022-01-17 00:55:10 +0100

    Express

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+    res.send('Home Page')
+});
+app.get('/hola-mundo', function (req, res) {
+    res.send('Hola mundo desde su respectiva ruta')
+});
+
+//? El asterisco * es un comidín para cualquier ruta que no hayamos definido.
+app.get('*', function (req, res) {
+    res.send('404 | Page not found')
+});
 
 app.listen(8080)
