55f4d6587dcb77f5ad388745bd78e7ac9960e091
Daniel Españadero <developer@daniel-espanadero.com>
2022-01-17 01:54:05 +0100

    SEO friendly

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const app = express()
+const port = 8080;
 
-app.get('/', function (req, res) {
-    res.send('Home Page')
+// Servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
 });
-app.get('/hola-mundo', function (req, res) {
-    res.send('Hola mundo desde su respectiva ruta')
+
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html')
 });
+
+app.get('/elements', (req, res) =>{
+    res.sendFile(__dirname + '/public/elements.html')
+});
+
 
 //? El asterisco * es un comidín para cualquier ruta que no hayamos definido.
+
 app.get('*', function (req, res) {
-    res.send('404 | Page not found')
+    res.sendFile(__dirname + '/public/404.html')
 });
 
-app.listen(8080)
+app.listen(port, () => {
+    console.log(`Escuchando en http://localhost:${port}`)
+});
